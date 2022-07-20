9dd5dca34cac010ca5a4ea0ac1c90b11bd390fb6
Lucas Locks <lucaslocks37@gmail.com>
2022-07-19 21:27:55 -0300

    desafiofinal

--- Projeto/calculadorades.py
+++ Projeto/calculadorades.py
@@ -1,22 +1,21 @@
+exp = []
 class Calculadora:
     result = 0
 
-    def __init__(self, op, num1, num2) -> None:
+    def __init__(self, num1) -> None:
         self.num1 = num1
-        self.num2 = num2
-        self.op = op
-        _f = str(num1) + str(self.op) + str(num2)
+        _f = str(num1)
         self.result = eval(_f)
-        
 
-num1 = str(input("Digite o numero: "))
-op = input('Informe o operador: ').upper()
-num2 = str(input("Digite o numero: "))
-calculadora1 = Calculadora(op, num1, num2)
-exp = (f'Resultados: {calculadora1.num1} {calculadora1.op} {calculadora1.num2} = {calculadora1.result}\n')
 
-resp = str(input("Quer continuar (S ou N): "))
-if resp == 'N':
-    print(f'{num1} {op} {num2} = {calculadora1.result}')
-if resp == 'S':
-    chamaclasse
+def calcula():
+    num1 = str(input("Digite a expressão: "))
+    calculadora1 = Calculadora(num1)
+    exp.append(f'{calculadora1.num1} = {calculadora1.result}')
+    resp = str(input("Quer continuar (S ou N): ")).upper()
+    if resp == 'N':
+        print(exp)
+    if resp == 'S':
+        calcula()
+
+calcula()
